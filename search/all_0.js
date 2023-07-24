2f2bea1a48fe96383c308e9fde617ea92cbbfbbc
AleRo96 <87674633+AleRo96@users.noreply.github.com>
2023-07-24 18:20:11 +0200

    Delete search directory

--- search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['clbk_5fodom_0',['clbk_odom',['../go__to__point_8py.html#a36304a9f313b0579f7fc69fa01695524',1,'go_to_point']]]
-];
--- search/functions_6.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['planning_28',['planning',['../go__to__point_8py.html#ad80fa73a15e1a5576fea98b4958dcef4',1,'go_to_point']]]
-];
